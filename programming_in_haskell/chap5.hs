63a1b7a6f9af92a0ae968fc8f46218fc6c622afc
santiweide <ggy_778@outlook.com>
2024-09-03 21:33:17 +0200

    add notations add another exercise in chap5

--- programming_in_haskell/chap5.hs
+++ programming_in_haskell/chap5.hs
@@ -1,5 +1,8 @@
-import Data.Char
+-- casesarEncoder 
+-- casear encoder is a encoder that reads a string and get a string which each letter is moved forwards 3 steps.
 
+
+import Data.Char
 
 isSmall :: Char -> Bool
 isSmall ch = 'a' <= ch && ch <= 'z'
@@ -27,5 +30,25 @@ casesarEncoder :: Int -> String -> String
 casesarEncoder step str = [getCasearCh step ch | ch <- str]
 
 
+-- freq
+-- freq is a function that get the occurancy by % for each letter in a string
+-- Capital letter and Smaller letter are considered the same letter
 
+getPercent :: Int -> Int -> Double
+getPercent a b = 100 * (fromIntegral a / fromIntegral b)
+
+getLower :: Char -> Char
+getLower ch = case isCap ch of
+            False -> ch
+            True -> chr (ord ch - ord 'a' + ord 'A')
+
+getAllLower :: String -> String
+getAllLower str = [getLower ch | ch <- str]
+
+count :: Char -> String -> Int
+count ch str = length [s | s<-str, s == ch]
+
+freq :: String -> [Double]
+freq str = [getPercent (count ch str) (length n) | ch <- ['a'..'z']  ]
+                where n = getAllLower str
 
