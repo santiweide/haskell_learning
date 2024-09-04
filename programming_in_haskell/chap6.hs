9a922aeddadbc51e5e4f9e685bd46ece40c5f7ce
santiweide <ggy_778@outlook.com>
2024-09-04 19:31:42 +0200

    finished chapter 6 recursive: text and exercise; especially merge sort, quick sort and insert sort

--- programming_in_haskell/chap6.hs
+++ programming_in_haskell/chap6.hs
@@ -46,10 +46,94 @@ reverse2 [] = []
 reverse2 (x:xs) = (reverse2 xs) ++ [x]
 
 -- fib
+fib :: Int -> Int
+fib n = case n <=2 of
+    True -> case n <=0 of
+        True -> 0
+        False -> 1
+    False -> fib (n-1) + fib (n-2) 
 
 -- even
+even2 :: Int -> Bool
+even2 0 = True
+even2 n = odd2 (n-1)
+
 -- odd
+odd2 0 = False
+odd2 n = even2 (n-1)
+
 -- evens
+-- get all numbers in a list that on the even place
+evens :: [Int] -> [Int]
+evens [] = []
+evens (x:xs) = x:(odds xs)
+
 -- odds
+odds :: [Int] -> [Int]
+odds [] = []
+odds (x:xs) = evens xs
+
+
 -- init
+init2 :: [a] -> [a]
+init2 [] = []
+init2 [_] = []
+init2 (x:xs) = x : init2 xs
+
+
+--- Exercises
+-- sumdown
+sumdown :: Int -> Int
+sumdown n = case n<=0 of
+    True -> 0
+    False -> n + sumdown (n-1)
+
+-- (^)
+(@) :: Double -> Int -> Double
+a @ 0 = 1
+1 @ n = 1
+a @ n = case n < 0 of
+    True -> 1 / (a @ (-n) )
+    False -> a * (a @ (n-1))
+
+-- euclid
+euclid :: Int -> Int -> Int 
+euclid a b = case a < b of 
+    True -> euclid (b-a) a
+    False -> case a == b of
+        True -> a
+        False -> euclid (a-b) b
+
+-- merge
+-- merge two ordered list (increasing)
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] a = a
+merge a [] = a
+merge (x:xs) (y:ys) = case x < y of 
+    True  -> (x : (merge xs (y:ys) ))
+    False -> (y : (merge (x:xs) ys ))
+
+-- merge sort
+getFront :: Int -> [a] -> [a]
+getFront 0 _ = []
+getFront n [] = []
+getFront n (x:xs) = [x] ++ getFront (n-1) xs
+
+getBack :: Int -> [a] -> [a]
+getBack 0 _ = []
+getBack n [] = []
+getBack n xs = reverse (take n (reverse xs))
+
+msort :: Ord a => [a] -> [a]
+msort xs = case length xs of
+        0 -> []
+        1 -> xs
+        2 -> merge [head xs] (tail xs)
+        otherwise -> merge (msort l) (msort r)
+            where   l = take len_l xs
+                    r = getBack len_r xs
+                    len_l = (length xs) `div` 2
+                    len_r =  (length xs) - len_l
+
+
 
